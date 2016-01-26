cfbb5793df356c0a7ec46dedba4c3b4e79a6ad61
ggalbit <galbit@ipnl.in2p3.fr>
2016-01-26 16:42:12 +0100

    Modifications of the binning function (now manage the binary width and the overflows for UNSIGNED and SIGNED (2's complement) representations. Range of the values have been optimized in the TCBuilder to get the best accuracy. Add new thresholds for the HW bit-accurate simulation.

--- src/TCBuilder.h
+++ src/TCBuilder.h
@@ -10,6 +10,7 @@
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/export.hpp>
 
+enum HW_SIGN_TYPE {UNSIGNED, SIGNED};
 
 enum SEC_TYPE {SEC_BARREL, SEC_HYBRID, SEC_ENDCAP};
 
@@ -40,7 +41,7 @@ class TCBuilder:public TrackFitter{
   double m_tabHybridThresholds[2][3][14][2];  //Hybrid LayMaxSeed1 = 1, LayMaxSeed2 = 2, LayMaxTestStub = 13
   double m_tabEndcapThresholds[4][5][16][2];  //Endcap LayMaxSeed1 = 3, LayMaxSeed2 = 4, LayMaxTestStub = 15
 
-  int m_nFractionnalPartWidth;
+  bool m_bHardwareSimulation;
   int m_nMissingHits;
 
   /**
@@ -50,7 +51,7 @@ class TCBuilder:public TrackFitter{
   void addThresholds(int, int, int, SEC_TYPE, double, double);
   void getThresholds(int, int, int, SEC_TYPE, double[]);
   char transcodeLayer(Hit *);
-  double binning(double, int);
+  double binning(double, int, int, HW_SIGN_TYPE);
   void alignScore(Hit& , Hit& , Hit& , double []);
 
  public:
